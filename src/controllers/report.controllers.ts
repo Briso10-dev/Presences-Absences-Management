172d4fcfb58b594c211052ec6dc1d68a0a8f0854
Briso10 <Kouambrice10@gmail.com>
2024-08-04 09:44:56 +0100

    montly absenceReports

--- src/controllers/report.controllers.ts
+++ src/controllers/report.controllers.ts
@@ -41,7 +41,43 @@ const generateReport = {
             sendError(res,error)
         }
    },   
-    
+   absenceReport : async (req:Request,res:Response)=>{
+    try {
+        const {absenceID,period} = req.body
+        //finding an employee's presence if yes forcefully that employee exists
+        const absence = await prisma.absence.findFirst({
+            select:{
+                absenceID:true,
+                date:true,
+            },
+            where:{
+                absenceID
+            }
+        })
+        if (!absence)
+            return res.status(HttpCode.NOT_FOUND).json({ msg: "You actually were not present" })
+        const report = await prisma.report.create({
+            data: {
+                content: "This is me again",
+                period
+            }
+        })
+        if (!report)
+            return res.status(HttpCode.INTERNAL_SERVER_ERROR).json({ msg: "no report generated" })
+           //updating presence's model
+           await prisma.absence.update({
+            where:{
+                absenceID
+            },
+            data:{
+                repAbsenceID : report.reportID
+            }
+        })
+        return res.status(HttpCode.OK).json({ msg: "Report generated" })
+    } catch (error) {
+        sendError(res,error)
+    }
+},   
 }
 
 export default generateReport
--- src/routes/report.routes.ts
+++ src/routes/report.routes.ts
@@ -4,5 +4,6 @@ import generateReport from "../controllers/report.controllers";
 const routerReport = Router()
 
 routerReport.get("/attendance",generateReport.attendanceReport)
+routerReport.get("/absence",generateReport.absenceReport)
 
 export default routerReport
